fc158dec0a4e4bccee68d110d4b1879e2430bad9
Neitralov <maxim.neitralov@gmail.com>
2023-03-19 19:17:39 +0700

    Добавление новых пространств имен, подключаемых глобально

--- ScheduleBot.WebApp/Usings.cs
+++ ScheduleBot.WebApp/Usings.cs
@@ -1,11 +1,12 @@
 ﻿global using static ScheduleBot.WebApp.NLogLogger;
 
-global using File = System.IO.File;
+global using static System.IO.File;
 global using System.IO.Compression;
 
 global using ScheduleBot.Enums;
 global using ScheduleBot.Structs;
 global using ScheduleBot.DataBase;
+global using ScheduleBot.WebApp.Models;
 
 global using Telegram.Bot;
 global using Telegram.Bot.Exceptions;
@@ -16,6 +17,15 @@ global using Telegram.Bot.Types.InputFiles;
 
 global using System.Diagnostics;
 
+global using System.ComponentModel.DataAnnotations;
+global using System.Security.Claims;
+
 global using Microsoft.EntityFrameworkCore;
+
+global using Microsoft.AspNetCore.Mvc;
+global using Microsoft.AspNetCore.Mvc.RazorPages;
+global using Microsoft.AspNetCore.Authorization;
+global using Microsoft.AspNetCore.Authentication;
+global using Microsoft.AspNetCore.Authentication.Cookies;
 
 global using NLog;
