4144935acc547c4b76467a32c13e0dcf5d077f3d
Neitralov <maxim.neitralov@gmail.com>
2023-03-19 19:14:46 +0700

    Класс переименован

---
+++ ScheduleBot.WebApp/Models/Log.cs
@@ -0,0 +1,8 @@
+namespace ScheduleBot.WebApp.Models;
+
+public class Log
+{
+    public string DateTime { get; init; } = string.Empty;
+    public string Status { get; init; } = string.Empty;
+    public string Message { get; init; } = string.Empty;
+}
